5fce372280df3e4081434989564ccb8ed5720ec4
Mathilde Bret <bretmathilde@orange.fr>
2022-07-07 18:28:20 +0200

    création de la fonction pour faire apparaître les messages d'erreurs en asynchrone si le formulaire n'est pas bien rempli lors du click sur le bouton d'envoie.

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -31,13 +31,16 @@ closeModal.addEventListener("click", () => {
 
 
 
-// prénom : plus de 2 caractères & pas vide & pas de chiffres
+// NOM & PRENOM VALIDES
 
-var nameRegex = /^[a-zA-Z\-]+$/;
-let firstname = document.getElementById("first");
+var nameRegex = /^[a-zA-Z\-]+$/
+let firstname = document.getElementById("first")
+let lastname = document.getElementById("last")
 let errorFirstname = document.getElementById("error-firstname")
+let errorLastname = document.getElementById("error-lastname")
 
-firstname.addEventListener("change", function(event) {
+// Au click sur l'input
+firstname.addEventListener("change", function(event) { 
   // Si la valeur récupérée dans l'input est inférieur à 2
   if(event.target.value.length < 2){
     errorFirstname.textContent = "Veuillez saisir plus de 2 caractères."
@@ -52,50 +55,72 @@ firstname.addEventListener("change", function(event) {
   }  
 });
 
+function isFirstNameValid(){
+  if(firstname.value.length < 2){
+    errorFirstname.textContent = "Veuillez saisir plus de 2 caractères.";
+    return false;
+  }
+  // Sinon si la valeur récupérée ne correspond pas à nameRegex
+  else if(!firstname.value.match(nameRegex)){
+    errorFirstname.textContent = "Les chiffres et caractères spéciaux ne sont pas autorisés.";
+    return false;
+  }
+  // Sinon
+  else{
+    errorFirstname.textContent = "";
+    return true;
+  }  
+}
 
-
-// nom : plus de 2 caractères & pas vide & pas de chiffres
-
-let lastname = document.getElementById("last");
-let errorLastname = document.getElementById("error-lastname")
-
+// Au click sur l'input
 lastname.addEventListener("change", function(event) {
   // Si la valeur récupérée dans l'input est inférieur à 2
   if(event.target.value.length < 2){
     errorLastname.textContent = "Veuillez saisir plus de 2 caractères."
+    return false;
   }
   // Sinon si la valeur récupérée ne correspond pas à nameRegex
   else if(!event.target.value.match(nameRegex)){
     errorLastname.textContent = "Les chiffres et caractères spéciaux ne sont pas autorisés."
+    return false;
   }
   // Sinon
   else{
-    errorLastname.textContent = ""
+    errorLastname.textContent = "";
+    return true
   }  
 });
 
-
-// valeurs numériques tournois
-
-let nbrTournoi = document.getElementById("quantity")
-let errorQuantity = document.getElementById("error-quantity")
-
-nbrTournoi.addEventListener("change", function(event){
-  if(isNaN(event.target.value)){
-    errorQuantity.textContent = "Veuillez saisir une valeur numérique."
+// Fonction qui regroupe nom et prénom
+function isLastNameValid(){
+  if(lastname.value.length < 2){
+    errorLastname.textContent = "Veuillez saisir plus de 2 caractères.";
+    return false;
   }
+  // Sinon si la valeur récupérée ne correspond pas à nameRegex
+  else if(!lastname.value.match(nameRegex)){
+    errorLastname.textContent = "Les chiffres et caractères spéciaux ne sont pas autorisés.";
+    return false;
+  }
+  // Sinon
   else{
-    errorQuantity.textContent = ""
-  }
-})
+    errorLastname.textContent = "";
+    return true;
+  }  
+}
 
 
-// adresse mail valide
 
-var emailRegex = /^([a-zA-Z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
-let email = document.getElementById("email");
+
+
+
+// EMAIL VALIDE
+
+var emailRegex = /^([a-zA-Z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+let email = document.getElementById("email")
 let errorEmail = document.getElementById("error-email")
 
+// Au click sur l'input
 email.addEventListener("change", function(event) {
 
   // Si la valeur récupérée ne correspond pas à nameRegex
@@ -109,12 +134,28 @@ email.addEventListener("change", function(event) {
 });
 
 
-// date de naissance valide
+function isEmailValid(){
+  if(!email.value.match(emailRegex)){
+    errorEmail.textContent = "Adresse email non valide"
+  }
+  // Sinon
+  else{
+    errorEmail.textContent = ""
+  }  
+}
+
+
+
+
+
+// DATE DE NAISSANCE VALIDE
 
 var birthdateRegex = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/;
 let birthdate = document.getElementById("birthdate");
 let errorBirthdate = document.getElementById("error-birthdate")
 
+
+// Au click sur l'input
 birthdate.addEventListener("change", function(event) {
 
   // Si la valeur récupérée ne correspond pas à nameRegex
@@ -127,33 +168,14 @@ birthdate.addEventListener("change", function(event) {
   }  
 });
 
-
-
-
-
-// Boutons radios 
-
-
-
-function isValideCity(){
-  
-  var chx = document.querySelectorAll("input[type=radio]");
-  let errorInput = document.getElementById("error-input");
-  console.log(chx[1].checked);
-  for(var i = 0; i < chx.length; i++){
-    console.log(chx[i].checked);
-    if(chx[i].checked){
-    errorInput.textContent = "";
-   console.log('no error')
-    return true;
-    } 
-  
-    
-   }
-   // attention : dans une boucle, on ne met pas de else, on met le reste de la condition en dehors de la boucle.
-   errorInput.textContent = "Sélectionner au moins une ville";
-      return false;
-
+function isBirthdateValid(){
+  if(!birthdate.match(birthdateRegex)){
+    errorBirthdate.textContent = "Date de naissance non valide"
+  }
+  // Sinon
+  else{
+    errorBirthdate.textContent = ""
+  }  
 }
 
 
@@ -161,18 +183,97 @@ function isValideCity(){
 
 
 
+// NOMBRE DE TOURNOI VALIDE 
 
-// Retirer la fonction par défaut de l'envoi du formulaire 
+let quantity = document.getElementById("quantity")
+let errorQuantity = document.getElementById("error-quantity")
+
+// Au click sur l'input
+quantity.addEventListener("change", function(event){
+  if(isNaN(event.target.value) || event.target.value == ''){
+    errorQuantity.textContent = "Veuillez saisir une valeur numérique."
+  }
+  else{
+    errorQuantity.textContent = ""
+  }
+})
+
+function isQuantityValid(){
+  if(isNaN(quantity.value) || quantity.value == ''){
+    errorQuantity.textContent = "Veuillez saisir une valeur numérique."
+  }
+  else{
+    errorQuantity.textContent = ""
+  }
+}
+
+
+
+
+
+// CHOIX DE VILLE VALIDE 
+
+var city = document.querySelectorAll("input[type=radio]");
+let errorInput = document.getElementById("error-input");
+
+
+function isCityValid(){
+
+  for( i = 0; i < city.length; i++){
+    if(city[i].checked){
+      errorInput.textContent = "";
+      return true;
+    }    
+  }
+   
+  // attention : dans une boucle, on ne met pas de else, on met le reste de la condition en dehors de la boucle.
+  errorInput.textContent = "Sélectionner au moins une ville";
+  return false;
+}
+
+
+
+function isConditionAccepted(){
+  var condition = document.getElementById("checkbox1");
+  let errorCondition = document.getElementById("error-condition");
+ 
+    if(condition.checked){
+      errorCondition.textContent = "";
+      return true;  
+    }    
+    else{
+      errorCondition.textContent = "Veuillez accepter les conditions d'utilisation";
+      return false;
+    }
+}
+  
+
+
 
 
 function validate(event) {
-  event.preventDefault();
+  event.preventDefault(); // on retire la fonction par défaut de l'envoie du formulaire
   
-
-  if(isValideCity()) {
+  setTimeout(function() {  // on créé un event loop avec set Timeout pour que le code s'exécute en asynchrone
+    isFirstNameValid()
+  }, 0);
+    isLastNameValid(), 0;
+    isEmailValid(), 0;
+    isQuantityValid(), 0;
+    isCityValid(), 0;
+    isConditionAccepted(), 0;
+   
+  
+  if(isFirstNameValid() && isLastNameValid() && isEmailValid() && isQuantityValid() && isCityValid() && isConditionAccepted()) {
     console.log("le formulaire valide ");
+    return true
+  }
+  else{
+    return false
   }
 }
+
+
 
 
 
