a07db98bbbb12375ebed69b93bd095d5f93a4f09
mathos <mathilde.meltingk@gmail.com>
2022-07-07 14:05:12 +0200

    fonction pour cocher au moins un bouton radio

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -44,7 +44,7 @@ firstname.addEventListener("change", function(event) {
   }
   // Sinon si la valeur récupérée ne correspond pas à nameRegex
   else if(!event.target.value.match(nameRegex)){
-    errorFirstname.textContent = "Les chiffres ne sont pas autorisés."
+    errorFirstname.textContent = "Les chiffres et caractères spéciaux ne sont pas autorisés."
   }
   // Sinon
   else{
@@ -66,7 +66,7 @@ lastname.addEventListener("change", function(event) {
   }
   // Sinon si la valeur récupérée ne correspond pas à nameRegex
   else if(!event.target.value.match(nameRegex)){
-    errorLastname.textContent = "Les chiffres ne sont pas autorisés."
+    errorLastname.textContent = "Les chiffres et caractères spéciaux ne sont pas autorisés."
   }
   // Sinon
   else{
@@ -90,7 +90,76 @@ nbrTournoi.addEventListener("change", function(event){
 })
 
 
-// Sélectionner 1 bouton radio
+// adresse mail valide
+
+var emailRegex = /^([a-zA-Z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+let email = document.getElementById("email");
+let errorEmail = document.getElementById("error-email")
+
+email.addEventListener("change", function(event) {
+
+  // Si la valeur récupérée ne correspond pas à nameRegex
+  if(!event.target.value.match(emailRegex)){
+    errorEmail.textContent = "Adresse email non valide"
+  }
+  // Sinon
+  else{
+    errorEmail.textContent = ""
+  }  
+});
+
+
+// date de naissance valide
+
+var birthdateRegex = /^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/;
+let birthdate = document.getElementById("birthdate");
+let errorBirthdate = document.getElementById("error-birthdate")
+
+birthdate.addEventListener("change", function(event) {
+
+  // Si la valeur récupérée ne correspond pas à nameRegex
+  if(!event.target.value.match(birthdateRegex)){
+    errorBirthdate.textContent = "Date de naissance non valide"
+  }
+  // Sinon
+  else{
+    errorBirthdate.textContent = ""
+  }  
+});
+
+
+
+
+
+// Boutons radios 
+
+
+
+function isValideCity(){
+  
+  var chx = document.querySelectorAll("input[type=radio]");
+  let errorInput = document.getElementById("error-input");
+  console.log(chx[1].checked);
+  for(var i = 0; i < chx.length; i++){
+    console.log(chx[i].checked);
+    if(chx[i].checked){
+    errorInput.textContent = "";
+   console.log('no error')
+    return true;
+    } 
+  
+    
+   }
+   // attention : dans une boucle, on ne met pas de else, on met le reste de la condition en dehors de la boucle.
+   errorInput.textContent = "Sélectionner au moins une ville";
+      return false;
+
+}
+
+
+
+
+
 
 
 // Retirer la fonction par défaut de l'envoi du formulaire 
@@ -98,7 +167,11 @@ nbrTournoi.addEventListener("change", function(event){
 
 function validate(event) {
   event.preventDefault();
-  console.log("Au clic le formulaire ne s'envoi pas")
+  
+
+  if(isValideCity()) {
+    console.log("le formulaire valide ");
+  }
 }
 
 
